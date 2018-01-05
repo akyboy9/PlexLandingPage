786ae9d1a8fbcf62c3fe949cc21c81eaa1252a96
akyboy9 <ivatury.akshay@gmail.com>
2018-01-04 18:07:49 -0600

    fixed login stuffz

--- app/views.py
+++ app/views.py
@@ -1,32 +1,24 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from app import app
 from plexapi.myplex import MyPlexAccount
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
-@app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 def index_login():
-    username = request.form['text']
-    password = request.form['pass']
-    password = text.upper()
-    username = text.upper()
-    if login_sucesful(username, password):
-		return redirect('/login')
-    return redirect('/index')
+	if request.method == 'POST':
+		username = request.form['text']
+		password = request.form['pass']
+		password = password.upper()
+		username = username.upper()
+		if username == 'USERNAME':
+			if password == 'PASSWORD':
+				return redirect('/login')
+		return redirect('/')
 
-def login_succesful(username, password):
-	master_acc = MyPlexAccount(account_1, password_1)
-	master_friends = master_acc.users()
-	master_friends = str(master_friends)
-	login_acc = MyPlexAccount(username, password)
-	login_user = login_acc.username
-	login_user = str(login_user)
-	#check if its in friends list
-	if login_user in master_friends:
-		return True
-	else:
-		return False
+@app.route('/login')
+def login():
+	return render_template('landing.html')
+
