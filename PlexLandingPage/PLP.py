6886204d063f0ebe96715e70154e794f8e30a8c8
zhoffm <zhoffm@gmail.com>
2018-01-07 01:53:57 -0600

    removed the installable python module. It doesn't really make sense to do it like that in this case. All functions have been consolidated to PLP.py

---
+++ PlexLandingPage/PLP.py
@@ -0,0 +1,54 @@
+from flask import request, render_template, redirect, Flask
+from plexapi.myplex import MyPlexAccount
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index', methods=['GET','POST'])
+def index_login():
+    if request.method == 'POST':
+        username = request.form['text']
+        password = request.form['pass']
+        password = password.upper()
+        username = username.upper()
+        if username == 'USERNAME':
+            if password == 'PASSWORD':
+                return redirect('/home')
+        return redirect('/')
+
+
+@app.route('/home')
+def login():
+    return render_template('landing.html')
+
+
+@app.route('/landingToRequest', methods=['GET','POST'])
+def landing_to_request():
+    if request.method == 'POST':
+        return redirect('/request_plex')
+
+
+@app.route('/request_plex')
+def request_plex():
+    return render_template('request.html')
+
+
+@app.route('/process_request', methods=['GET', 'POST'])
+def process_request():
+    if request.method == 'POST':
+        requested = request.form['text_2']
+        requested = requested.upper()
+        print requested
+        return redirect('/request_plex')
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
--- PlexLandingPage/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from views import PlexLandingPage
-
-app = Flask(__name__)
-
